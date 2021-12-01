63a61fa7ec904a49a495e04c045832dc8c284d50
Adam Miller <adam@adammiller.name>
2021-11-30 21:45:24 -0800

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,31 @@
 fun main() {
+    fun countIncreases(measurements: List<Int>) = measurements.filterIndexed { i, v ->
+        i != 0 && v > measurements[i - 1]
+    }.count()
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val measurements = input.map { it.toInt() }
+        return countIncreases(measurements)
     }
 
+    fun computeSlidingWindows(rawMeasurements: List<Int>) =
+        rawMeasurements.mapIndexed { i, v ->
+            if (rawMeasurements.size - i < 3) {
+                Int.MIN_VALUE
+            } else {
+                listOf(v, rawMeasurements[i + 1], rawMeasurements[i + 2]).sum()
+            }
+        }
+
     fun part2(input: List<String>): Int {
-        return input.size
+        val rawMeasurements = input.map { it.toInt() }
+        val slidingWindows = computeSlidingWindows(rawMeasurements)
+        return countIncreases(slidingWindows)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
 
     val input = readInput("Day01")
     println(part1(input))
